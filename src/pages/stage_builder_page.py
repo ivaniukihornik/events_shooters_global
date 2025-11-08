596f8d1fdbb4715cf56134ec996648b26e3e238c
Ihor Ivaniuk <ivaniukihornik@gmail.com>
2025-11-08 09:41:42 +0200

    Deleted unused elements

--- src/pages/stage_builder_page.py
+++ src/pages/stage_builder_page.py
@@ -12,12 +12,9 @@ class StageBuilderPage(BasePage):
         self.limited_tablet_alert = self.page.locator('*[role=alertdialog]')
         self.limited_tablet_alert_confirm_button = self.page.locator('div[role=alertdialog] button')
         self.loading_screen = self.page.locator('section[id=unity-loading-screen]')
-        self.builder_screen = self.page.locator('#unity-app-root')
         self.builder_canvas = self.page.frame_locator('#unity-app-root iframe').locator('canvas[id=unity-app-canvas]')
         self.DEFAULT_ONBOARDING_ALERT_CLOSE_BUTTON_X = 1344
         self.DEFAULT_ONBOARDING_ALERT_CLOSE_BUTTON_Y = 146
-        self.ACTUAL_CANVAS_WIDTH = None
-        self.ACTUAL_CANVAS_HEIGHT = None
 
     def confirm_limited_tablet_alert(self) -> 'StageBuilderPage':
         try:
@@ -37,14 +34,8 @@ class StageBuilderPage(BasePage):
         self.loading_screen.wait_for(state='hidden', timeout=BUILDER_LOADING_TIMEOUT_MS)
         return self
 
-    def write_canvas_size(self) -> None:
-        self.ACTUAL_CANVAS_WIDTH = int(self.builder_canvas.get_attribute('width'))
-        self.ACTUAL_CANVAS_HEIGHT = int(self.builder_canvas.get_attribute('height'))
-
     def close_onboarding_alert(self) -> 'StageBuilderPage':
         time.sleep(3)  # НЕ ХОТІВ, ЗАСТАВИЛИ ((((
-        if self.ACTUAL_CANVAS_WIDTH is None or self.ACTUAL_CANVAS_HEIGHT is None:
-            self.write_canvas_size()
         self.builder_canvas.click(position=Position(
             x=self.DEFAULT_ONBOARDING_ALERT_CLOSE_BUTTON_X,
             y=self.DEFAULT_ONBOARDING_ALERT_CLOSE_BUTTON_Y
